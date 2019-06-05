26fe4982b5b5287fecf71dc310f87511ce3817ec
jekill <1jorge.echeverria@gmail.com>
2019-06-05 08:51:27 -0400

    v2

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,6 +6,8 @@ import contextoProblema.Marca;
 public class Main {
     public static void main(String[] args) {
         Automovil automovil = new Automovil(Marca.FORD.getMarca());
-        System.out.println(automovil.getMarca());
+        System.out.println("La marca es " + automovil.getMarca());
+        automovil.setMarca(Marca.NISSAN.getMarca());
+        System.out.println("La marca es " + automovil.getMarca());
     }
 }
